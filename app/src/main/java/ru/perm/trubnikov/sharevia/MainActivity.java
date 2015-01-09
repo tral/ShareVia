5d7c5b789866ed9eec5e57cdbfe0cea03f55d4f3
tralchonok <tralmail@gmail.com>
2015-01-09 23:55:49 +0530

    -

--- app/src/main/java/ru/perm/trubnikov/sharevia/MainActivity.java
+++ app/src/main/java/ru/perm/trubnikov/sharevia/MainActivity.java
@@ -132,7 +132,7 @@ public class MainActivity extends ActionBarActivity {
             if (!found)
                 return;
 
-            startActivity(Intent.createChooser(share, "Select"));
+            startActivity(Intent.createChooser(share, ""));
         }
     }
 
